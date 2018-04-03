cc8d9920f225f21a6fc28787642ce17325daabec
Delasa Aghamirzaie <Delasa@Delasas-MacBook-Pro.local>
2018-04-03 14:13:52 -0700

    polished code

--- src/load_data.py
+++ src/load_data.py
@@ -1,3 +1,8 @@
+'''
+Written by Delasa Aghamirzaie for NCBI Hackaton
+process SRA files and creates LD counts
+'''
+
 from __future__ import print_function
 from optparse import OptionParser
 import subprocess
@@ -13,6 +18,7 @@ def main():
     parser.add_option('-i', dest = 'prefix', default=None, type='str', help='Add prefix [Default: %default]')
     parser.add_option('-v', dest = 'vcf', default=False,  action='store_true', help='VCF input file: %default]')
     parser.add_option('-s', dest = 'sra', default=False,  action='store_true', help='SRA_id: %default]')
+    parser.add_option('-c', dest='clean', default=False, action='store_true', help='clean intermediate files: %default]')
 
     ## TODO: add genome version and species
 
@@ -36,22 +42,13 @@ def main():
         save_name = '%s_%s' %(options.prefix, save_name)
 
     out_dir = options.out_dir.strip('/') + '/' + save_name + '/'
-    print (out_dir)
     if not os.path.isdir(out_dir):
         os.mkdir(out_dir)
         print ('created %s' %out_dir)
 
-    #check log file:
- #   print (save_name)
 
-    # naming_dic = set_naming_convention(save_name, out_dir)
-    # naming_dic['input_file'] = input_file
-    # print (naming_dic)
-    #
-    ###
+    runner(input_file, save_name, out_dir, options)
 
-    runner(input_file, save_name, options)
-  #  run_hisat(naming_dic)
 
     ###to do: clean after each run
 
@@ -65,36 +62,37 @@ def runner(input_file, save_name, out_dir, options):
         vcf_run_stat = True
 
 
-    naming_dic = set_naming_convention(save_name)
+    naming_dic = set_naming_convention(save_name, out_dir)
     naming_dic['input_type'] = input_type
+    naming_dic['input_file'] = input_file
 
     if options.vcf:
         naming_dic['sorted_markd_recal_vcf'] = input_file
     #    print (naming_dic)
     check_file_existence(file_path=naming_dic['log_file'], create=True)
-
-    log_file = open(out_dir + naming_dic['log_file'])
+    log_file = open(naming_dic['log_file'], 'a+')
     log_file.write('~~~~~~ started new run for : %s ~~~~~~~~\n' % save_name)
 
     # TODO clean after each successful run
-    if options.vcf != None:
+    if options.vcf:
         # TODO
         # process vcf file via bedtools intersect
         vcf_run_stat = True
+
     else:
+        print ('processing SRA file using custom pipeline')
         ### do steps to create SRA file from SRA id
         log_file.write(save_name)
         log_file.write("\n")
 
-        print(input_file, save_name)
+        print('save_name: %s' %save_name)
         log_file.write('started running HISAT2 \n')
-        bam_file = '%s%s.sort.bam' % (out_dir, save_name)
 
         #### ~~~~~~~~~~~~~~ run HISAT2 ~~~~~~~~~~~~~~~~~~~
-        if not os.path.exists(bam_file):
+        if not check_file_existence(naming_dic['sorted_bam']):
             run_hisat(naming_dic)
 
-        if not os.path.exists(bam_file):
+        if not check_file_existence(naming_dic['sorted_bam'], create=False):
             log_file.write('HISAT2 run was unsuccessfull\n')
             raise NameError('HISAT2 run was  unsuccessfull')
         else:
@@ -144,6 +142,9 @@ def runner(input_file, save_name, out_dir, options):
             log_file.write('Generating LD counts was unsuccessfull\n')
             raise NameError('Generating LD counts was unsucessful')
 
+    ###
+    log_file.write('~~~~~~~~~~~~~~~~  Preprocessing data finished successfully  ~~~~~~~~~~~~~~~~~~~~~~~~`\n')
+    print ('Preprocessing data finished successfully')
     ### ~~~~~~~~~~~~~~~~~
 
 def check_file_existence(file_path, create=False):
@@ -157,8 +158,6 @@ def check_file_existence(file_path, create=False):
     else:
         return False
 
-
-
 def set_naming_convention(save_name, out_dir):
     '''
     :param save_name:
@@ -170,7 +169,7 @@ def set_naming_convention(save_name, out_dir):
         'log_file': '%s%s.log' %(out_dir,save_name),
         'sorted_bam':'%s%s.sort.bam' %(out_dir,save_name),
         'sorted_markd_bam': '%s%s.sort.markd.bam' %(out_dir,save_name),
-        'sorted_marked_metrics': '%s%s.sort.markd.metrics.bam' %(out_dir, save_name),
+        'sorted_marked_metrics': '%s%s.sort.markd.metrics.txt' %(out_dir, save_name),
         'sorted_markd_recal_table':  '%s%s.sort.markd.recal.table' %(out_dir,save_name),
         'sorted_markd_recal_bam': '%s%s.sort.markd.recal.bam' %(out_dir,save_name),
         'sorted_markd_recal_vcf': '%s%s.sort.markd.recal.vcf.gz' %(out_dir,save_name),
@@ -179,7 +178,6 @@ def set_naming_convention(save_name, out_dir):
     }
 
     return (naming_dic)
-
 
 def run_hisat(naming_dic):
 
@@ -191,41 +189,39 @@ def run_hisat(naming_dic):
             print (tempfile.gettempdir())
             hisat_cmd = 'hisat2 -p ${ALIGN_THREADS} -x /opt/grch38/Homo_sapiens_assembly38 --sra-acc %s --rg-id %s --rg SM:%s --rg PL:ILLUMINA| samtools sort -@ ${SORT_THREADS} > %s -T %s' %(naming_dic['input_file'], naming_dic['input_file'], naming_dic['input_file'], naming_dic['sorted_bam'], t)
             print (hisat_cmd)
-     #   subprocess.call(hisat_cmd, shell=True)
-
+            subprocess.call(hisat_cmd, shell=True)
 
 def mark_duplicates(naming_dic):
         #/opt/gatk-4.0.3.0/gatk MarkDuplicates --INPUT ${SRR}.sort.bam --OUTPUT ${SRR}.sort.markd.bam --METRICS_FILE ${SRR}.sort.markd.metrics.bam
         cmd = '/opt/gatk-4.0.3.0/gatk MarkDuplicates --INPUT %s --OUTPUT %s --METRICS_FILE %s' %(naming_dic['sorted_bam'], naming_dic['sorted_markd_bam'], naming_dic['sorted_marked_metrics'])
         print (cmd)
-     #   subprocess.call(cmd, shell=True)
+        subprocess.call(cmd, shell=True)
+
 def base_recalibrator(naming_dic):
     ## BaseRecalibrator
     #/opt/gatk-4.0.3.0/gatk BaseRecalibrator -R /opt/grch38/Homo_sapiens_assembly38.fasta --known-sites /opt/grch38/dbsnp_138.hg38.vcf.gz --known-sites /opt/grch38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz -I ${SRR}.sort.markd.bam -O ${SRR}.sort.markd.recal.table
-    cmd = '/opt/gatk-4.0.3.0/gatk BaseRecalibrator -R /opt/grch38/Homo_sapiens_assembly38.fasta --known-sites /opt/grch38/dbsnp_138.hg38.vcf.gz --known-sites /opt/grch38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz -I %s -O %s' %(naming_dic['sorted_marked_bam'], naming_dic['sorted_markd_recal_table'])
+    cmd = '/opt/gatk-4.0.3.0/gatk BaseRecalibrator -R /opt/grch38/Homo_sapiens_assembly38.fasta --known-sites /opt/grch38/dbsnp_138.hg38.vcf.gz --known-sites /opt/grch38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz -I %s -O %s' %(naming_dic['sorted_markd_bam'], naming_dic['sorted_markd_recal_table'])
     print (cmd)
-#    subprocess.call(cmd, shell=True)
-
+    subprocess.call(cmd, shell=True)
 
 def print_reads(naming_dic):
     #/opt/gatk-4.0.3.0/gatk ApplyBQSR -R /opt/grch38/Homo_sapiens_assembly38.fasta -I ${SRR}.sort.markd.bam --bqsr-recal-file ${SRR}.sort.markd.recal.table -O ${SRR}.sort.markd.recal.bam
     cmd = '/opt/gatk-4.0.3.0/gatk ApplyBQSR -R /opt/grch38/Homo_sapiens_assembly38.fasta -I %s --bqsr-recal-file %s -O %s'%(naming_dic['sorted_markd_bam'],naming_dic['sorted_markd_recal_table'],naming_dic['sorted_markd_recal_bam'])
     print (cmd)
-#    subprocess.call(cmd, shell=True)
+    subprocess.call(cmd, shell=True)
 
 def haplotype_caller(naming_dic):
     #/opt/gatk-4.0.3.0/gatk HaplotypeCaller -R /opt/grch38/Homo_sapiens_assembly38.fasta --dbsnp /opt/grch38/dbsnp_138.hg38.vcf.gz -I ${SRR}.sort.markd.recal.bam -O ${SRR}.sort.markd.recal.vcf.gz
     cmd = '/opt/gatk-4.0.3.0/gatk HaplotypeCaller -R /opt/grch38/Homo_sapiens_assembly38.fasta --dbsnp /opt/grch38/dbsnp_138.hg38.vcf.gz -I %s -O %s' %(naming_dic['sorted_markd_recal_bam'],naming_dic['sorted_markd_recal_vcf'])
     print (cmd)
-#    subprocess.call(cmd, shell=True)
-
+    subprocess.call(cmd, shell=True)
 
 def bedtools_intersect(naming_dic):
 
     #'/home/ubuntu/bin/bedtools intersect -a /home/ubuntu/ldetect_GRCh38/EUR_ldetect.bed -b ${SRR}.sort.markd.recal.vcf.gz -c | sort -k1,1V -k2,2n > ${SRR}.count'
     cmd = '/home/ubuntu/bin/bedtools intersect -a /home/ubuntu/ldetect_GRCh38/EUR_ldetect.bed -b %s -c | sort -k1,1V -k2,2n > %s' %(naming_dic['sorted_markd_recal_vcf'], naming_dic['ld_counts'])
     print (cmd)
-    #    subprocess.call(cmd, shell=True)
+    subprocess.call(cmd, shell=True)
 
 # __main__
 ################################################################################
